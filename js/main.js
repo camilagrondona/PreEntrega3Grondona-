bf17d75b9c5a2896deac7f568af15043663cf06a
Camila Grondona <grondonacamila@gmail.com>
2023-08-28 21:13:16 +0200

    funciones globales

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ const productos = [
         categoria: "Marvel"
     },
     {
-        id: 3,  
+        id: 3,
         nombre: "Funko Pop! Marvel Guardianes de la Galaxia 3 - Groot",
         precio: 30990,
         imagen: "./img/groot.jpg",
@@ -26,7 +26,7 @@ const productos = [
         id: 4,
         nombre: "Funko Pop! Marvel Wanda Vision - Vision 50s Blanco y Negro",
         precio: 44000,
-        imagen: "./img/vision50s.jpg", 
+        imagen: "./img/vision50s.jpg",
         categoria: "Marvel"
     },
     {
@@ -85,8 +85,80 @@ const productos = [
         imagen: "./img/mandogrogu.jpg",
         categoria: "Star Wars"
     },
-    ];
-    
+];
+/*Funciones del Carrito */
+
+// Función dibujar Carrito
+
+const dibujarCarrito = () => {
+
+    listaCarrito.innerHTML = '';
+    carrito.forEach((productosDisponibles) => {
+        const { imagen, nombre, cantidad, precio, id } = productosDisponibles
+        let body = document.createElement("tr")
+
+        body.className = "producto__carrito"
+
+        body.innerHTML = `
+    <th><img id="fotoProductoCarrito" src="${imagen}" class="card-img-top" style="width:40%; height: 30%"</th>
+    <td>${nombre}</td>
+    <td>${cantidad}</td>
+    <td>${precio / cantidad}</td>
+    <td>${precio}</td>
+    <td>
+    <button id="+${id}" class="btn btn-success">+</button>
+    <button id="-${id}" class="btn btn-danger">-</button>
+    </td>
+    `
+
+        listaCarrito.append(body)
+
+        const btnAgregar = document.getElementById(`+${id}`)
+        const btnRestar = document.getElementById(`-${id}`)
+
+        btnAgregar.addEventListener("click", () => aumentarCantidad(id))
+        btnRestar.addEventListener("click", () => restarCantidad(id))
+
+    });
+
+    dibujarFooter();
+};
+
+// Función dibujar Footer Carrito 
+
+const dibujarFooter = () => {
+
+    if (carrito.length > 0) {
+        footCarrito.innerHTML = ""
+
+        let footer = document.createElement("tr")
+
+        footer.innerHTML = `
+    <th><b>Totales:</b></th>
+    <td></td>
+    <td>${generarTotales().cantidadTotal}</td>
+    <td></td>
+    <td>${generarTotales().costoTotal}</td>
+    `
+
+        footCarrito.append(footer)
+    } else {
+        footCarrito.innerHTML = "<h3> No agregaste productos al carrito</h3>"
+    }
+
+}
+
+// Función generar totales Carrito
+
+const generarTotales = () => {
+    const costoTotal = carrito.reduce((total, { precio }) => total + precio, 0)
+    const cantidadTotal = carrito.reduce((total, { cantidad }) => total + cantidad, 0)
+
+    return {
+        costoTotal: costoTotal,
+        cantidadTotal: cantidadTotal
+    }
+}
 
 // Inicio
 
@@ -102,7 +174,7 @@ const generarCardsProductos = (productos) => {
     divProductos.innerHTML = "";
 
     productos.forEach((producto) => {
-// Card productos
+        // Card productos
         let card = document.createElement("div");
         card.className = "producto";
         card.innerHTML = `
@@ -119,7 +191,7 @@ const generarCardsProductos = (productos) => {
         divProductos.appendChild(card);
 
         const btnComprar = document.getElementById(`comprar${producto.id}`)
-        // Función del carrito
+        // Función del carrito comprar producto
         btnComprar.addEventListener("click", () => comprarProducto(producto.id))
     });
 };
@@ -128,11 +200,12 @@ const generarCardsProductos = (productos) => {
 
 JSON.parse(sessionStorage.getItem("carrito")) === null && sessionStorage.setItem("carrito", JSON.stringify([]))
 
-let carrito = JSON.parse(sessionStorage.getItem("carrito"))
+// Agregar productos al carrito 
+
 const comprarProducto = (idProducto) => {
-    const productosDisponibles = productos.find ((producto) => producto.id === idProducto)
+    const productosDisponibles = productos.find((producto) => producto.id === idProducto)
     const productoCarrito = carrito.find((producto) => producto.id === idProducto)
-    if (productoCarrito === undefined){
+    if (productoCarrito === undefined) {
         const nuevoProductoCarrito = {
             id: productosDisponibles.id,
             nombre: productosDisponibles.nombre,
@@ -140,7 +213,7 @@ const comprarProducto = (idProducto) => {
             imagen: productosDisponibles.imagen,
             cantidad: 1
         }
-        
+
         carrito.push(nuevoProductoCarrito)
 
         sessionStorage.setItem("carrito", JSON.stringify(carrito))
@@ -155,7 +228,63 @@ const comprarProducto = (idProducto) => {
     carrito = JSON.parse(sessionStorage.getItem("carrito"))
 
     alert(`Compraste el producto ${productosDisponibles.nombre}`)
+
+    // Aumentar la cantidad en el carrito 
+
+    const aumentarCantidad = (id) => {
+        const indexProductoCarrito = carrito.findIndex((producto) => producto.id === id)
+        const precio = carrito[indexProductoCarrito].precio / carrito[indexProductoCarrito].cantidad
+
+        carrito[indexProductoCarrito].cantidad++
+        carrito[indexProductoCarrito].precio = precio * carrito[indexProductoCarrito].cantidad
+
+        sessionStorage.setItem("carrito", JSON.stringify(carrito))
+        dibujarCarrito()
+
+    }
+
+    // Restar la cantidad en el carrito 
+
+    const restarCantidad = (id) => {
+        const indexProductoCarrito = carrito.findIndex((producto) => producto.id === id)
+        const precio = carrito[indexProductoCarrito].precio / carrito[indexProductoCarrito].cantidad
+
+        carrito[indexProductoCarrito].cantidad--
+        carrito[indexProductoCarrito].precio = precio * carrito[indexProductoCarrito].cantidad
+
+        if (carrito[indexProductoCarrito].cantidad === 0) {
+            carrito.splice(indexProductoCarrito, 1)
+        }
+
+        sessionStorage.setItem("carrito", JSON.stringify(carrito))
+        dibujarCarrito()
+    }
 }
+
+//Dibujar tabla carrito 
+
+document.addEventListener("DOMContentLoaded", () => {
+    dibujarCarrito();
+});
+
+let carrito = JSON.parse(sessionStorage.getItem("carrito"))
+
+const listaCarrito = document.getElementById("items")
+const footCarrito = document.getElementById("totales")
+const btnCarrito = document.getElementById("btnCarrito")
+const carritoTable = document.getElementById("carrito")
+
+// Evento: si hago click sobre el carrito aparece la tabla
+
+btnCarrito.addEventListener("click", () => {
+    if (carritoTable.style.display === "block") {
+        carritoTable.style.display = "none"
+    } else {
+        carritoTable.style.display = "block"
+        dibujarCarrito()
+    }
+})
+
 // Usuarios
 
 let dataBaseUsers = [
