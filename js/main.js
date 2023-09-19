f6b35e805947ba77fe3ca2510bd3d3c460098e70
Camila Grondona <grondonacamila@gmail.com>
2023-09-19 17:09:54 +0200

    Generar card productos

--- js/main.js
+++ js/main.js
@@ -1,101 +1,59 @@
-// Array productos
-
-const productos = [
-    {
-        id: 1,
-        nombre: "Funko Pop! Marvel Avengers - Ironman",
-        precio: 30990,
-        imagen: "./img/ironman.jpg",
-        categoria: "Marvel"
-    },
-    {
-        id: 2,
-        nombre: "Funko Pop! Marvel Avengers - Spiderman No Way Home",
-        precio: 30990,
-        imagen: "./img/spiderman.jpg",
-        categoria: "Marvel"
-    },
-    {
-        id: 3,
-        nombre: "Funko Pop! Marvel Guardianes de la Galaxia 3 - Groot",
-        precio: 30990,
-        imagen: "./img/groot.jpg",
-        categoria: "Marvel"
-    },
-    {
-        id: 4,
-        nombre: "Funko Pop! Marvel Wanda Vision - Vision 50s Blanco y Negro",
-        precio: 44000,
-        imagen: "./img/vision50s.jpg",
-        categoria: "Marvel"
-    },
-    {
-        id: 5,
-        nombre: "Funko Pop! Peliculas: Avatar The Way of the Water - Jake Sully",
-        precio: 30990,
-        imagen: "./img/jakesully.jpg",
-        categoria: "Peliculas"
-    },
-    {
-        id: 6,
-        nombre: "Funko Pop! Peliculas: Harry Potter - Dobby",
-        precio: 30990,
-        imagen: "./img/dobby.jpg",
-        categoria: "Peliculas"
-    },
-    {
-        id: 7,
-        nombre: "Funko Pop! Peliculas: Harry Potter - Harry con Hedwig Plataforma 9 3/4",
-        precio: 46000,
-        imagen: "./img/harryhedwig.jpg",
-        categoria: "Peliculas"
-    },
-    {
-        id: 8,
-        nombre: "Funko Pop! Peliculas: Suicide Squad - Harley Quinn",
-        precio: 30990,
-        imagen: "./img/harleyquinn.jpg",
-        categoria: "Peliculas"
-    },
-    {
-        id: 9,
-        nombre: "Funko Pop! Star Wars  - Chewbacca",
-        precio: 30990,
-        imagen: "./img/chewbacca.jpg",
-        categoria: "Star Wars"
-    },
-    {
-        id: 10,
-        nombre: "Funko Pop! Star Wars  - Darth Vader",
-        precio: 44000,
-        imagen: "./img/darthvader.jpg",
-        categoria: "Star Wars"
-    },
-    {
-        id: 11,
-        nombre: "Funko Pop! Star Wars The Mandalorian - Grogu",
-        precio: 30990,
-        imagen: "./img/grogu.jpg",
-        categoria: "Star Wars"
-    },
-    {
-        id: 12,
-        nombre: "Funko Pop! Star Wars The Mandalorian - Mando con Grogu",
-        precio: 46000,
-        imagen: "./img/mandogrogu.jpg",
-        categoria: "Star Wars"
-    },
-];
-
 const divProductos = document.getElementById("productos")
 const listaCarrito = document.getElementById("items")
 const footCarrito = document.getElementById("totales")
 const btnCarrito = document.getElementById("btnCarrito")
 const carritoTable = document.getElementById("carrito")
 
-let productosDisponibles = productos
+let productosDisponibles = [];
 
-//Funciones del Carrito:
+// Función para cargar los productos desde el archivo JSON
+const cargarProductos = () => {
+    fetch('./db/productos.json')
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('No se pudo cargar el archivo JSON de productos.');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            productosDisponibles = data;
+            generarCardsProductos(productosDisponibles);
+        })
+        .catch((error) => {
+            console.error('Error al cargar los productos:', error);
+        });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    cargarProductos();
+})
+
+// Generar card productos 
+    const generarCardsProductos = (productosDisponibles) => {
+    divProductos.innerHTML = "";
+
+    productosDisponibles.forEach((producto) => {
+        let card = document.createElement("div");
+        card.className = "producto";
+        card.innerHTML = `
+    <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="${producto.imagen}" alt="Card image cap">
+    <div class="card-body">
+    <p class="card-title">${producto.nombre}</p>
+    <p class="card-text">Categoria: ${producto.categoria}</p>
+    <p class="card-text">Precio: <b>$${producto.precio}</b></p>
+    <button id="comprar${producto.id}" class="btn btn-primary">Comprar</button>
+    </div>
+    </div>`;
+
+        divProductos.appendChild(card);
+
+        const btnComprar = document.getElementById(`comprar${producto.id}`)
+        btnComprar.addEventListener("click", () => comprarProducto(productosDisponibles, producto.id))
+    })
+}
+
+/*Funciones del Carrito*/
 
 // Función dibujar Carrito
 
@@ -127,7 +85,7 @@ const dibujarCarrito = () => {
     });
 
     dibujarFooter();
-};
+}
 
 // Función dibujar Footer Carrito 
 
@@ -164,38 +122,6 @@ const generarTotales = () => {
         cantidadTotal: cantidadTotal
     }
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-    // Función para crear las cards de mis productos
-    generarCardsProductos(productosDisponibles)
-})
-
-const generarCardsProductos = (productosDisponibles) => {
-    divProductos.innerHTML = "";
-
-    // Card productos 
-
-    productosDisponibles.forEach((producto) => {
-        let card = document.createElement("div");
-        card.className = "producto";
-        card.innerHTML = `
-    <div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="${producto.imagen}" alt="Card image cap">
-    <div class="card-body">
-    <p class="card-title">${producto.nombre}</p>
-    <p class="card-text">Categoria: ${producto.categoria}</p>
-    <p class="card-text">Precio: <b>$${producto.precio}</b></p>
-    <button id="comprar${producto.id}" class="btn btn-primary">Comprar</button>
-    </div>
-    </div>`;
-
-        divProductos.appendChild(card);
-
-// Función del carrito comprar producto
-        const btnComprar = document.getElementById(`comprar${producto.id}`)
-        btnComprar.addEventListener("click", () => comprarProducto(producto.id))
-    });
-};
 
 // Aumentar la cantidad en el carrito 
 
@@ -252,7 +178,7 @@ btnCarrito.addEventListener("click", () => {
 
 // Agregar productos al carrito 
 
-const comprarProducto = (idProducto) => {
+const comprarProducto = (productos, idProducto) => {
     let productosDisponibles = productos.find((producto) => producto.id === idProducto)
     const productoCarrito = carrito.find((producto) => producto.id === idProducto)
     if (productoCarrito === undefined) {
